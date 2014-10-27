b9284f111fc74a3beb114be6bfb298ba00bf8201
Christopher(gizmo385) <cachapline8@gmail.com>
2014-10-27 16:07:33 -0700

    Graph creation and edge addition tests

--- graphs/test/graphs/graph_test.clj
+++ graphs/test/graphs/graph_test.clj
@@ -4,6 +4,24 @@
 
 (def empty-graph (createGraph))
 
+(deftest graph-creation
+  (testing "Creating an empty graph"
+    (let [g (createGraph)
+          h (createGraph '())]
+      (are [x y] (= x y)
+           (.order g)   0
+           (.order h)   0
+           (.size g)    0
+           (.size h)    0)))
+  (testing "Creating a graph with a single vertex"
+    (let [g (createGraph "a" "1")]
+      (is (= (.order g) 1))
+      (is (= (.size g) 0))))
+  (testing "Variable argument graph creation"
+    (let [g (createGraph "a" "1" "b" "2" "c" "3")]
+      (is (= (.order g) 3))
+      (is (= (.size g) 0)))))
+
 (deftest empty-graph-tests
   (testing "Empty graph has an empty vertex set"
     (is (= 0 (.order empty-graph))))
@@ -21,6 +39,23 @@
       (let [h (.addVertex (.addVertex (.addVertex g "a" "1") "b" "2") "c" "3")]
         (is (= 4 (.order h)))
         (is (= 0 (.size h)))))))
+
+(deftest adding-edges
+  (let [g (createGraph "a" "1" "b" "2" "c" "3")
+        h (.addEdge g "a" "b")
+        k (.addEdge h "a" "c")]
+    (testing "Adding some simple edges"
+      (are [x y] (= x y)
+           (.order g) 3
+           (.order h) 3
+           (.order k) 3
+           (.size g)  0
+           (.size h)  1
+           (.size k)  2))
+    (testing "Self loops are disallowed and throw an exception"
+      (is (thrown? IllegalArgumentException (.addEdge g "a" "a"))))
+    (testing "Duplicate edges don't get added"
+      (is (= (.size (.addEdge h "a" "b")) 1)))))
 
 (deftest removing-vertices
   (let [g (.addVertex (.addVertex (.addVertex empty-graph "a" "1") "b" "2") "c" "3")

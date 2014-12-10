03538f4eedc0975c9bf909b153b099cbc7767a44
Christopher(gizmo385) <cachapline8@gmail.com>
2014-12-10 16:53:41 -0700

    Everything but move and joinGame implemented

--- ttt-server/src/ttt_server/core/handler.clj
+++ ttt-server/src/ttt_server/core/handler.clj
@@ -16,27 +16,15 @@
 (declare myGame)
 (declare move)
 
-(defrecord Player
-  [player_id      ; The unique identifier for this player
-   game_id        ; The unique identifier for the game that this player is in
-   player_type])  ; The symbol for this player, "X" or "O"
-
-(defrecord Game
-  [player_1       ; The id of the first player in the game
-   player_2       ; The id of the second player in the game
-   game_id        ; The id for this game
-   current_turn   ; The id for the player whose turn it is
-   game_state     ; The game state
-   board])        ; A map of the game cells
-
-(def players (atom {}))
-(def games (atom {}))
+(def players (atom {})) ; Maps player ids to player records
+(def games (atom {}))   ; Maps game ids to game records
 
 (defroutes app-routes
   (GET "/" []
-       (fn [client_data]
-         (let [query_args (:params client_data)
-               player_id (-> client_data :session/key str)]
+       (fn [request]
+         (let [query_args (:params request)
+               player_id (-> request :session/key str)]
+           ; We will dispatch based on the command in the query string
            (case (:cmd query_args)
              "login"    (login player_id)
              "getGames" (getGames query_args)
@@ -47,7 +35,12 @@
              (str "Unrecognized arguments: " (json/write-str query_args))))))
   (GET "/secret-data" [] #(:session/key %1))
   (GET "/all-players" [] (fn [x] (json/write-str @players)))
-  (route/not-found "Not Found"))
+  (route/not-found #(format "Not Found: \"%s\"" (:uri %1))))
+
+(defn json-response [message next-title next]
+  {:status 200
+   :headers {"Content-Type" "application/json"}
+   :body (json/write-str {:message message (keyword next-title) next})})
 
 (defn fill-board [board_size fill_item]
   "Creates a map which maps the numbers from (1..board_size inclusive) to the string null"
@@ -58,23 +51,64 @@
       board)))
 
 (defn login [player_id]
+  "Logs in the user with the specified player ID"
   (if (not-nil? (get @players player_id))
-    (format "Welcome back %s!" player_id)
-    (let [player (Player. player_id nil nil)]
+    (json-response "Player Already Logged In" "playerid" player_id)
+    (let [player (create-player player_id)]
       (swap! players assoc player_id player)
-      (format "Welcome new player %s" player_id))))
+      (json-response "New Player Logged In" "playerid" player_id))))
+
+(defn create-player [player_id]
+  "Creates a player with a player id"
+  {:player_id       player_id
+   :game_id         nil
+   :player_type     (rand-nth ["X" "O"])
+   :last_modified   (System/currentTimeMillis)})
+
+(defn create-game [player1_id game_id]
+  "Creates a game where the specified id is the first player"
+  (let [player (get @players player1_id)]
+    (if (not-nil? player)
+      {:game_id     game_id
+       :game_state  "open"
+       :players     [player]
+       :you         player
+       :player_turn player
+       :moves       []
+       :board       (fill-board 9 "null")
+       :winner      nil}
+      nil)))
 
 (defn getGames [_]
-  (json/write-str @games))
+  "Returns all currently open games"
+  (json-response "Open Games" "data" (filter #(= "open" (:game_state %1)) (vals @games))))
 
-(defn newGame [player_id query_args]
-  "newGame")
+(defn newGame [player_id _]
+  "Creates a new game and associates the players with them"
+  (let [player      (get @players player_id)
+        old_game    (get @games (:game_id player))
+        game_id     (gensym)
+        new_player  (assoc player :game_id game_id)]
+    (swap! players assoc player_id new_player)
+
+    ; Create the game and add it to the list of games
+    (let [game (create-game player_id game_id)]
+      (swap! games assoc (:game_id game) game))
+
+    ; Mark the old game as ended
+    (if (not-nil? old_game)
+      (swap! games assoc (:game_id old_game) (assoc old_game :game_state "ended")))
+    (json-response "New game created" "gameID" game_id)))
+
 
 (defn joinGame [player_id query_args]
   "joinGame")
 
-(defn myGame [player_id query_args]
-  "myGame")
+(defn myGame [player_id _]
+  "Returns the current game state for the game that the player is in"
+  (let [player  (get @players player_id)
+        game    (get @games (:game_id player))]
+    (json-response (format "Game state for Game ID: %s" (:game_id game)) "data" game)))
 
 (defn move [player_id query_args]
   (str "player with id \"" player_id "\" move"))

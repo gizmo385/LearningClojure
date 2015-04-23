e0f33fa642c85c3fef4d36fe4e829a09a5a5eb34
Christopher Chapline <cachapline8@gmail.com>
2015-04-23 00:24:21 -0700

    Added function to resolve collision between two particles. Full collision handling next step

--- n-bodies/src/n_bodies/universe.clj
+++ n-bodies/src/n_bodies/universe.clj
@@ -38,6 +38,41 @@
   (let [new-particles (map (partial calculate-force-for-particle universe) (universe :particles))]
     (assoc universe :particles (into [] new-particles))))
 
+(defn- pow
+  "Raises the number to the power of the exponent."
+  [number exponent]
+  (reduce * (repeat exponent number)))
+
+(defn- calculate-dx
+  "Calculates the new dx value for this particle after colliding with the other particle."
+  [this other]
+  (/ (+
+      (* (other :x) (pow (- (other :x) (this :x)) 2))
+      (* (other :y) (- (other :x) (this :x)) (- (other :y) (this :y)))
+      (* (this :dx) (pow (- (other :y) (this :y)) 2))
+      (- (* (this :dy) (- (other :x) (this :x)) (- (other :y) (this :y)))))
+     (+ (pow (- (other :x) (this :x)) 2) (pow (- (other :y) (this :y))))))
+
+(defn- calculate-dy
+  [this other]
+  (/ (+
+      (* (other :dx) (- (other :x) (this :x)) (- (other :y) (this :y)))
+      (* (other :dy) (pow (- (other :y) (this :y)) 2))
+      (- (* (this :dx) (- (other :y) (this :y)) (- (other :x) (this :x))))
+      (* (this :dy) (pow (- (other :x) (this :x)) 2)))
+     (+ (pow (- (other :x) (this :x)) 2) (pow (- (other :y) (this :y)) 2))))
+
+(defn collide
+  "Resolves a collision between two particles. This function returns a 2-element vector that
+   contains the particles after the collisions have been resolved. The first and second elements of
+   the vector will correspond to p1 and p2 respectively."
+  [p1 p2]
+  (let [p1-new-dx (calculate-dx p1 p2)
+        p1-new-dy (calculate-dy p1 p2)
+        p2-new-dx (calculate-dx p2 p1)
+        p2-new-dy (calculate-dy p2 p1)]
+    [(assoc p1 :dx p1-new-dx :dy p1-new-dy) (assoc p2 :dx p2-new-dx :dy p2-new-dy)]))
+
 (defn step
   "Runs the universe for a single time-step, where DT time passes between snapshots."
   [universe DT]

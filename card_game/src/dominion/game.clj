60d6f5ac924b92b40e6ffe68aef71d59dfbaf60f
Christopher Chapline <cachapline8@gmail.com>
2016-05-10 02:28:11 -0700

    Free buy amount, hand limit. Documentation

--- card_game/src/dominion/game.clj
+++ card_game/src/dominion/game.clj
@@ -9,13 +9,29 @@
    :trash []})
 
 (def player-state-template
-  "The default template for the player's in the game"
+  "The default template for the player's in the game
+
+    * victory-points: Player with the most points wins.
+    * available-money: Used to buy cards during the buy phase.
+    * available-buys: The number of cards that can be bought during the buy phase.
+    * available-actions: The number of actions that can be played during the action phase.
+    * available-discards: The number of cards that can be discarded.
+    * available-trashes: The number of cards that can be trashed.
+    * free-buy-amount: The highest cost card that can be bought for free.
+    * hand-limit: The highest number of cards that can be held in the hand.
+    * name: The name of the player.
+    * deck: The player's current deck to draw from.
+    * hand: The cards that the player can play during their current turn.
+    * discard: The cards that the player has discarded during play. Shuffled into deck after deck
+               empties."
   {:victory-points 0
    :available-money 0
    :available-buys 1
    :available-actions 1
    :available-discards 0
    :available-trashes 0
+   :free-buy-amount 0
+   :hand-limit nil
    :name nil
    :deck []
    :hand []
@@ -51,6 +67,11 @@
   [game-state player-id]
   (get-in game-state [:players player-id :discard]))
 
+(defn get-hand-limit
+  "Retrieves the hand limit for a particular player"
+  [game-state player-id]
+  (get-in game-state [:players player-id :hand-limit]))
+
 ;;; Quick modification functions
 (defn add-card
   "Adds a card to a particular card location (:deck, :hand :discard) for a player."
@@ -61,6 +82,11 @@
   "Removes a card from a particular card location (:deck, :hand :discard) for a player."
   [game-state player-id location]
   (update-in game-state [:players player-id location] next))
+
+(defn set-hand-limit
+  "Retrieves the hand limit for a particular player"
+  [game-state player-id hand-limit]
+  (assoc-in game-state [:players player-id :hand-limit] hand-limit))
 
 ;;; Managing the card draws for the player
 (defn shuffle-discard
